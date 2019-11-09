986ad9a81d3d69d9f8c747d341011b8d8f6c61a8
Alexander Topchiy <topchiyau@gmail.com>
2019-11-09 15:01:22 +0300

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+/// This class represent UI of Login screen
+class LoginScreen extends StatefulWidget {
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
