c6110b9c4bcd1ecc3600e4d014f5aff842f832b1
Alexander Topchiy <topchiyau@gmail.com>
2019-11-09 19:19:10 +0300

    Implement UI of Login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,9 +14,77 @@ class LoginScreenState extends State<LoginScreen> {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Image.asset(
-            'assets/bg_login.png',
-            alignment: Alignment.topCenter,
+          Image.asset('assets/bg_login.png', alignment: Alignment.topCenter),
+          Positioned(
+            bottom: 50,
+            left: 30,
+            right: 30,
+            top: 100,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Вход',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 45, horizontal: 30),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          offset: Offset(0, 10),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          controller: TextEditingController(),
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Email'
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          controller: TextEditingController(),
+                          decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Пароль'
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        MaterialButton(
+                          onPressed: () {},
+                          color: Color.fromRGBO(155, 81, 224, 0.5),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          minWidth: double.infinity,
+                          height: 38,
+                          child: Text(
+                            'Войти',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
